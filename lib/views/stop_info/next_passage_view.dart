5f7d9d92bf8a6aa66c7a9f16066ccd94564ab14d
Bubulle <bubulle86@yahoo.fr>
2022-07-11 21:11:01 +0200

    Next passage view is working

--- lib/views/stop_info/next_passage_view.dart
+++ lib/views/stop_info/next_passage_view.dart
@@ -2,6 +2,7 @@ import 'package:better_bus_v2/data_provider/vitalis_data_provider.dart';
 import 'package:better_bus_v2/model/clean/bus_stop.dart';
 import 'package:better_bus_v2/views/common/line_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../model/clean/next_passage.dart';
 
@@ -18,10 +19,15 @@ class _NextPassagePageState extends State<NextPassagePage> {
 
   List<NextPassage>? nextPassages;
 
-  Future<List<NextPassage>> refreshData() async {
+  Future<List<NextPassage>> initData() async {
+    if (nextPassages != null) {
+      return nextPassages!;
+    }
     nextPassages = await VitalisDataProvider.getNextPassage(widget.stop);
     return nextPassages!;
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -29,28 +35,63 @@ class _NextPassagePageState extends State<NextPassagePage> {
       width: double.infinity,
       height: double.infinity,
       child: FutureBuilder<List<NextPassage>>(
-        future: refreshData(),
+        future: initData(),
+        initialData: nextPassages,
         builder: (BuildContext context, AsyncSnapshot<List<NextPassage>> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             if (snapshot.hasError) {
-              print(snapshot.error);
               return const Center(
                 child: Text("Error"),
               );
             } else if (snapshot.hasData) {
-              nextPassages = snapshot.data!;
-              return ListView.builder(
-                itemCount: nextPassages!.length,
-                itemBuilder: (context, index) => NextPassageWidget(nextPassages![index]),
-              );
+              return NextPassageListWidget(nextPassages!, widget.stop);
             }
           }
-          return Container();
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       )
     );
   }
 }
+
+class NextPassageListWidget extends StatefulWidget {
+  const NextPassageListWidget(this.nextPassages, this.stop, {Key? key}) : super(key: key);
+
+  final List<NextPassage> nextPassages;
+  final BusStop stop;
+
+  @override
+  State<NextPassageListWidget> createState() => _NextPassageListWidgetState();
+}
+
+class _NextPassageListWidgetState extends State<NextPassageListWidget> {
+  late List<NextPassage> nextPassages = List.from(widget.nextPassages);
+
+
+  Future<void> refreshData() async {
+    List<NextPassage> result = await VitalisDataProvider.getNextPassage(widget.stop);
+    if (mounted) {
+      setState(() {
+        nextPassages = result;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return RefreshIndicator(
+      onRefresh: refreshData,
+      child: ListView.builder(
+        itemCount: nextPassages.length,
+        itemBuilder: (context, index) => NextPassageWidget(nextPassages[index]),
+      ),
+    );
+  }
+}
+
+
 
 class NextPassageWidget extends StatelessWidget {
   const NextPassageWidget(this.nextPassage, {Key? key}) : super(key: key);
@@ -58,10 +99,11 @@ class NextPassageWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String formattedTime = "${nextPassage.expectedTime.hour.toString().padLeft(2, "0")} : "
-        "${nextPassage.expectedTime.minute.toString().padLeft(2, "0")}";
+    String formattedTime = DateFormat.Hm().format(nextPassage.expectedTime.toLocal());
+    String minuteToWait = "${nextPassage.expectedTime.difference(DateTime.now()).inMinutes} min";
     return Container(
-      height: 50,
+      height: 55,
+      padding: EdgeInsets.symmetric(vertical: 2, horizontal: 5),
       decoration: BoxDecoration(
         border: Border(
           bottom: BorderSide(width: 1, color: Theme.of(context).primaryColor)
@@ -70,18 +112,36 @@ class NextPassageWidget extends StatelessWidget {
       child: Row(
         children: [
           LineWidget(nextPassage.line, 45),
-          Padding(
-            padding: const EdgeInsets.only(left: 8),
-            child: Text(
-              nextPassage.destination,
-              style: Theme.of(context).textTheme.headline5,
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: Text(
+                nextPassage.destination,
+                style: Theme.of(context).textTheme.headline5,
+                overflow: TextOverflow.fade,
+                maxLines: 1,
+                softWrap: false,
+              ),
             ),
           ),
-          Spacer(),
-          Column(
-            children: [
-              Text(formattedTime)
-            ],
+          Container(
+            child: nextPassage.realTime ? const Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Icon(Icons.wifi),
+            ) : null,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Column(
+              children: [
+                Text(minuteToWait,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),),
+                const Spacer(),
+                Text(formattedTime),
+              ],
+            ),
           )
         ],
       ),
