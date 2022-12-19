051ec5ab847ae2aa5f1a34bc79a0982f632bac6a
Bubulle <bubulle86@yahoo.fr>
2022-12-19 14:33:16 +0100

    Changing version provider

--- lib/data_provider/version_data_provider.dart
+++ lib/data_provider/version_data_provider.dart
@@ -1,12 +1,14 @@
 import 'package:better_bus_v2/data_provider/cache_data_provider.dart';
 import 'vitalis_data_provider.dart';
-const String version  = "0.1";
+import 'package:package_info_plus/package_info_plus.dart';
+
 const String versionURL = "https://pastebin.com/raw/0uFw7Vze";
 const CacheDataProvider cache = CacheDataProvider(key: "version", expiration: Duration(days: 1));
 
 class VersionDataProvider {
   static Future<String?> checkIfNewVersion() async {
     Map<String, dynamic> data = await VitalisDataProvider.sendRequest(Uri.parse(versionURL), cache: cache);
+    String version = (await PackageInfo.fromPlatform()).version;
     if (data["version"] != version) {
       return data["download-link"] as String;
     }
