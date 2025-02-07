46615a4468f16d815940695b5904db42d7a779a9
Bubulle <bubulle86@yahoo.fr>
2025-02-07 01:39:32 +0100

    More tests they'll fail

--- test/core_test.dart
+++ test/core_test.dart
@@ -12,8 +12,20 @@ void testNetwork(BusNetwork network) async {
     expect(network.isAvailable(), true);
   });
 
-  test("Test stations", () async {
-    expect(network.isAvailable(), true);
+  test("Test getStations", () async {
     expect(await network.getStations(), isNotEmpty);
+  });
+
+  test("Test getAllLines", () async {
+    expect(await network.getAllLines(), isNotEmpty);
+  });
+
+  test("Test getLine from stop", () async {
+    final stations = await network.getStations();
+    stations.shuffle();
+    final nd = stations.firstWhere((e) => e.name.startsWith("Notre-"));
+    expect(nd, isNotNull);
+    expect(await network.getPassingLines(stations.first), isNotEmpty);
+    expect(await network.getPassingLines(nd), isNotEmpty);
   });
 }
