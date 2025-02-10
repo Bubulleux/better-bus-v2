854ec1197d5ff22aaa4d1fe33f75abe2f37d6f33
Bubulle <bubulle86@yahoo.fr>
2025-02-10 19:55:15 +0100

    Fixing Test, Creating Full Provider

--- test/core_test.dart
+++ test/core_test.dart
@@ -2,37 +2,54 @@ import 'dart:io';
 
 import 'package:better_bus_v2/core/api_provider.dart';
 import 'package:better_bus_v2/core/bus_network.dart';
+import 'package:better_bus_v2/core/full_provider.dart';
 import 'package:better_bus_v2/core/gtfs_downloader.dart';
 import 'package:better_bus_v2/core/gtfs_provider.dart';
 import 'package:better_bus_v2/core/models/gtfs/gtfs_path.dart';
+import 'package:better_bus_v2/core/models/station.dart';
 import 'package:test/test.dart';
 import 'package:test_descriptor/test_descriptor.dart' as d;
 
 void main() async {
-  // TODO: Need to be tested
+
+  final downloader = GTFSDataDownloader.vitalis(
+      GTFSPaths.broken()
+  );
+
+  final gtfs = GTFSProvider(provider: downloader);
+  ApiProvider api = ApiProvider.vitalis();
+
+  setUp(() async{
+    await d.dir("gtfs", [
+      d.dir("download"),
+      d.dir("extract"),
+    ]).create();
+    downloader.paths = GTFSPaths("${d.sandbox}/gtfs/download/gtfs.zip", "${d.sandbox}/gtfs/extract/");
+  });
+
+  // // TODO: Need to be tested
   group("Test Vitalis Api reponse", () {
-    ApiProvider api = ApiProvider.vitalis();
     testNetwork(api);
     testInfoTrafficProvider(api);
   });
 
   group("Test Vitalis GTFS", () {
-    final downloader = GTFSDataDownloader.vitalis(
-        GTFSPaths.broken()
-    );
-
-    final network = GTFSProvider(provider: downloader);
-
-    setUp(() async{
-      await d.dir("gtfs", [
-        d.dir("download"),
-        d.dir("extract"),
-      ]).create();
-      downloader.paths = GTFSPaths("${d.sandbox}/gtfs/download/gtfs.zip", "${d.sandbox}/gtfs/extract/");
-    });
 
     testGTFSDownloader(downloader);
-    testNetwork(network);
+    testNetwork(gtfs);
+  });
+
+  group("Test Networks Equality", () {
+    testProviderEquality(api, gtfs);
+  });
+
+  group("Test Full Provider", () {
+    final provider = FullProvider(api: api, gtfs: gtfs);
+    testNetwork(provider);
+  });
+
+  group("Faild Test", () {
+    failTest();
   });
 }
 
@@ -88,10 +105,66 @@ void testGTFSDownloader(GTFSDataDownloader downloader) {
       return;
     }
     expect(data.stations, isNotEmpty);
-    //expect(data.stops, isNotEmpty);
     expect(data.stopTime, isNotEmpty);
     expect(data.routes, isNotEmpty);
     expect(data.calendar, isNotNull);
     expect(data.trips, isNotEmpty);
+  });
+}
+
+void testProviderEquality(ApiProvider api, GTFSProvider gtfs) {
+
+  test("Test Available", () {
+    expect(api.isAvailable(), isTrue);
+    expect(gtfs.isAvailable(), isTrue);
+  });
+
+  test("Test Station equality", () async {
+    final apiStations = await api.getStations();
+    final gtfsStations = await gtfs.getStations();
+
+    print("Api Missing Station");
+    for (var curStation in gtfsStations) {
+      if (apiStations.contains(curStation)) continue;
+      print(curStation);
+    }
+    expect(apiStations.length, (gtfsStations.length));
+    expect(apiStations, same(gtfsStations));
+  });
+
+  test("Test Line equality", () async {
+    final apiLines = await api.getAllLines();
+    final gtfsLines = await gtfs.getAllLines();
+
+    print("Api Missing Lines");
+    for (var curLine in gtfsLines.entries) {
+      if (apiLines.containsKey(curLine.key)) continue;
+      print(curLine.value);
+    }
+    expect(apiLines.length, (gtfsLines.length));
+    expect(apiLines, same(gtfsLines));
+  });
+
+  test("Station equality", () {
+    // final apiResponce = aw
+  });
+
+}
+
+void failTest() {
+  test("Need To Fail", () {
+    print("HAHAH");
+    expect(4, 20);
+    //expect(actual, matcher)
+  });
+  test("Need To Fail More", () {
+    print("HAHAH");
+    expect(false, isTrue);
+    //expect(actual, matcher)
+  });
+  test("Need To Fail too", () {
+    print("HAHAH");
+    expect([1, 2, 3], isEmpty);
+    //expect(actual, matcher)
   });
 }
