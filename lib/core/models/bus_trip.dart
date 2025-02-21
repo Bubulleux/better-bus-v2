f893d3af84ad3fcbfa322a3963734d4707fc13e7
Bubulle <bubulle86@yahoo.fr>
2025-02-21 01:51:41 +0100

    Fixing Model Graph

--- lib/core/models/bus_trip.dart
+++ lib/core/models/bus_trip.dart
@@ -3,10 +3,31 @@ import 'package:better_bus_v2/core/models/line_direction.dart';
 import 'package:better_bus_v2/core/models/station.dart';
 
 class BusTrip extends LineDirected {
-  BusTrip({
-    required LineDirection direction,
-    this.stopTimes,
-  }) : super(direction);
+  BusTrip(LineDirection direction, {required this.stopTimes})
+      : super(direction);
 
-  final Map<DateTime, Station>? stopTimes;
+  BusTrip.fromMap({
+    required LineDirection direction,
+    required Map<DateTime, Station> stopTimes,
+  }) : this(direction,
+            stopTimes: stopTimes.entries
+                .map((e) => TripStop.fromMapEntry(e))
+                .toList());
+
+  final List<TripStop> stopTimes;
+
+  @override
+  String toString() {
+    return 'BusTrip{direction: $direction, stopLength: ${stopTimes.length}';
+  }
+}
+
+class TripStop {
+  TripStop(this.time, this.station);
+
+  TripStop.fromMapEntry(MapEntry<DateTime, Station> entry)
+      : this(entry.key, entry.value);
+
+  final DateTime time;
+  final Station station;
 }
--- lib/core/models/gtfs/trip.dart
+++ lib/core/models/gtfs/trip.dart
@@ -1,9 +1,10 @@
-
 import 'package:better_bus_v2/core/models/bus_line.dart';
+import 'package:better_bus_v2/core/models/bus_trip.dart';
 import 'package:better_bus_v2/core/models/gtfs/direction.dart';
 import 'package:better_bus_v2/core/models/gtfs/line.dart';
 import 'package:better_bus_v2/core/models/gtfs/stop_time.dart';
 import 'package:better_bus_v2/core/models/line_direction.dart';
+import 'package:better_bus_v2/helper.dart';
 
 class GTFSTrip {
   late final LineDirection direction;
@@ -13,14 +14,20 @@ class GTFSTrip {
 
   BusLine get line => direction.line;
 
-
-  GTFSTrip(Map<String, String> row, this.stopTimes, GTFSLine line){
+  GTFSTrip(Map<String, String> row, this.stopTimes, GTFSLine line) {
     _routeID = int.parse(row["route_id"]!);
     serviceID = row["service_id"]!;
     direction = GTFSLineDirection.fromTripRow(row, line);
   }
 
-  // BusTrip toTrip() {
-  //
-  // }
+  BusTrip at(DateTime from) {
+    final date = from.atMidnight();
+    return BusTrip(direction,
+        stopTimes: stopTimes
+            .map((e) => TripStop(date.add(e.arival), e.station))
+            .toList());
+  }
+// BusTrip toTrip() {
+//
+// }
 }
