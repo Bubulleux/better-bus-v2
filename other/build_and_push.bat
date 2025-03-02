ff32cfb338f7ccf2e2b552ed57753e76a526a5b7
Bubulle <bubulle86@yahoo.fr>
2025-03-02 20:28:44 +0100

    Moving server and core to another repo

--- other/build_and_push.bat
+++
@@ -1,4 +0,0 @@
-
-flutter build apk --release
-
-adb -s R9PR203J3LJ push build\app\outputs\apk\release\app-release.apk /storage/self/primary/Download
--- packages/better_bus_core/lib/core.dart
+++
@@ -1,7 +0,0 @@
-export 'src/full_provider.dart';
-export 'src/api_provider.dart';
-export 'src/gtfs_provider.dart';
-export 'src/gtfs_downloader.dart';
-export 'src/cache_data_provider.dart';
-export 'src/models.dart';
-export 'src/helper.dart';
