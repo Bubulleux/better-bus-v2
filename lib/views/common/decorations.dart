c86165d533c3571ddc04a680742cf2ce5cbf1d50
Bubulle <bubulle86@yahoo.fr>
2025-04-20 16:00:40 +0200

    Updating simpleShadow

--- lib/views/common/decorations.dart
+++ lib/views/common/decorations.dart
@@ -21,6 +21,11 @@ class CustomDecorations {
 
   static final BorderRadius borderRadius = BorderRadius.circular(20);
   static final simpleShadow = [
-    BoxShadow(color: Colors.black38, offset: Offset(2, 2), blurRadius: 5, spreadRadius: 3)
+    const BoxShadow(
+    spreadRadius: 1,
+    blurRadius: 3,
+    offset: Offset(2, 2),
+    color: Colors.black38,)
+    // BoxShadow(color: Colors.black38, offset: Offset(2, 2), blurRadius: 5, spreadRadius: 3)
   ];
 }
